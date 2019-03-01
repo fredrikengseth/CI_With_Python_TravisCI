5c9348f1b440b5846c3bf014a9a516537a74ba48
Fredrik Engseth <fredrikengseth@gmail.com>
2019-03-01 20:49:45 +0100

    initial commit

---
+++ app/app-test.py
@@ -0,0 +1,18 @@
+import unittest
+from app import add_numbers
+
+
+class MyTest(unittest.TestCase):
+    def test_add_numbers(self):
+        self.assertEqual(add_numbers(1, 1), 2)
+        self.assertEqual(add_numbers(1, -1), 0)
+        self.assertEqual(add_numbers(0, 0), 0)
+        self.assertEqual(add_numbers(-1, -1), -2)
+        self.assertEqual(add_numbers(1.0, 1), 2)
+        self.assertEqual(add_numbers(1.1, 1.1), 2.2)
+        # 1 + 1 = 4
+        self.assertEqual(add_numbers(1, 2), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
